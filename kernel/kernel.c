5484a5761994c81da6182f7f140a0385da4cb975
marvol0 <murithimarvin09@gmail.com>
2024-07-29 16:51:57 +0300

    feat: add entry point for the kernel

---
+++ kernel/kernel.c
@@ -0,0 +1,10 @@
+#include "kernel.h"
+
+void kernel_main() {
+    const char *str = "Hello, kernel world!";
+    char *video_memory = (char*) 0xb8000;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        video_memory[i*2] = str[i];
+        video_memory[i*2+1] = 0x07;
+    }
+}
